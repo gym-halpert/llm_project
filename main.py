f5862e8175cc9964c5acd8c388322746cec4c809
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2026-01-17 21:23:29 -0700

    Successful function of API and token counts

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    try:
+        api_key
+    except NameError:
+        raise RuntimeError("API key not found...")
+        sys.exit(1)
+
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    try:
+        response.usage_metadata
+    except ValueError:
+        raise RuntimeError("No metadata found...")
+        sys.exit(1)
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
