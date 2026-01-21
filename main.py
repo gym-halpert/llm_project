598da04a26cb74491f8787d508aacf8cc169761f
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2026-01-20 22:25:41 -0700

    Added multiple messages and verbosity

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,8 +12,10 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 def main():
     try:
@@ -22,7 +25,7 @@ def main():
         sys.exit(1)
 
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt
+    model='gemini-2.5-flash', contents=messages
     )
 
     try:
@@ -31,10 +34,18 @@ def main():
         raise RuntimeError("No metadata found...")
         sys.exit(1)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
-    print(response.text)
+    if args.verbose:
+        print(f"\nUser prompt: {args.user_prompt}")
+        print(f"\nPrompt tokens: {prompt_tokens}")
+        print(f"\nResponse tokens: {response_tokens}\n")
+        print(response.text)
+    else:
+        print(f"\nUser prompt: {args.user_prompt}")
+        print(f"\n{response.text}\n")
+
 
 if __name__ == "__main__":
     main()
