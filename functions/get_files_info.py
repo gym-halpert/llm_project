993bfdda50c38927ff4018eecc18a6185a2c0ee2
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2026-01-22 21:47:51 -0700

    Revising functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,8 +5,12 @@ def get_files_info(working_directory, directory="."):
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
+    print(f"{working_dir_abs}\n{target_dir}\n{valid_target_dir}\n")
+
     dir_test = os.path.isdir(target_dir)
     listed_target = os.listdir(target_dir)
+
+    print(f"{dir_test}\n{listed_target}")
 
     if valid_target_dir == False:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
@@ -17,4 +21,4 @@ def get_files_info(working_directory, directory="."):
     for item in listed_target:
         size = os.path.getsize(item)
         file = os.path.isdir(item)
-        return f"- {item}: file_size={size} bytes, is_dir={file}"
+        print(f"- {item}: file_size={size} bytes, is_dir={file}")
