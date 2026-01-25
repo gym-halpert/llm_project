4c7a29738c6f77b755b35e7156c6623ac1ee3511
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2026-01-25 14:17:40 -0700

    Run python function passing tests

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path, args=None):
     try:
@@ -10,7 +11,7 @@ def run_python_file(working_directory, file_path, args=None):
             return f'Error: Cannot execute "{file_path}" as it is outside of the permitted working directory'
         if not os.path.isfile(target_file_path):
             return f'Error: "{file_path}" does not exist or is not a regular file'
-        if target_file_path.endswith(".py"):
+        if not target_file_path.endswith(".py"):
             return f'Error: "{file_path}" is not a Python file'
 
         command = ["python", target_file_path]
@@ -18,4 +19,22 @@ def run_python_file(working_directory, file_path, args=None):
         if args:
             command.extend(args)
 
-        completed = subprocess.run(commad, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=abs_working_dir, timeout=30)
+        completed = subprocess.run(command, capture_output=True, text=True, cwd=abs_working_dir, timeout=30)
+#        STDOUT = f'{completed.stdout.strip()}'
+#        STDERR = f'{completed.stderr.strip()}'
+
+        output = []
+
+        if completed.returncode != 0:
+            return f'Process completed with return code {completed.returncode}'
+
+        if not completed.stdout and not completed.stderr:
+            return f'No output produced'
+        else:
+            output.append(f'STDOUT: {completed.stdout.strip()}')
+            output.append(f'STDERR: {completed.stderr.strip()}')
+            output_string = "\n".join(output)
+        return output_string
+
+    except Exception as e:
+        return f'Error executing Python file: {e}'
